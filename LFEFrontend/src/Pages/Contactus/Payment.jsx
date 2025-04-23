88463426d9e7582c7ab9305bc6a9c06a6773f543
Sanjay Narvariya <sanjaynarvariya12@gmail.com>
2025-04-23 18:28:45 +0530

    Update Payment.jsx
    sdf

--- LFEFrontend/src/Pages/Contactus/Payment.jsx
+++ LFEFrontend/src/Pages/Contactus/Payment.jsx
@@ -110,63 +110,65 @@ const Payment = () => {
 
 
             <section>
-                <div className="container-fluid" style={{ marginTop: '10px', marginBottom:'-100px' }}>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="section1img" style={{ marginTop: '50px' }}>
-                                <img
-                                    src={Truck}
-                                    alt="Truck"
-                                    data-aos="fade-right"
-                                    data-aos-duration="3000"
-                                />
-                            </div>
-                        </div>
-                        {/* <!-- Payment Guidelines Section --> */}
-                        <div className="col-md-6">
-                            <div className="section2info">
-                                <div className="section2title">
-                                    <div class="payment-guidelines">
-                                    <h4>Payment Guidelines</h4>
-                                    <h1>Secure, Easy & Transparent Payment Options</h1>
-                                    <p>
-                                        We offer multiple secure payment methods to ensure a smooth experience. Please review the following guidelines before proceeding:
-                                    </p>
+  <div className="container-fluid" >
+    <div className="row">
+      {/* Truck Image */}
+      <div className="col-md-6">
+        <div className="section1img" style={{marginBottom:'90px'}}>
+          <img
+            src={Truck}
+            alt="Truck"
+            data-aos="fade-right"
+            data-aos-duration="3000"
+            style={{ borderRadius:'50px'}}
+          />
+        </div>
+      </div>
 
-                                    <ul>
-                                        <li><strong>Accepted Methods:</strong>
-                                            <span class="icons">
-                                                <img src={bhim1} alt="BHIM UPI" />
-                                                <img src={bhim2} alt="UPI" />
-                                                <img src={bhim3} alt="Visa" />
-                                                <img src={bhim4} alt="MasterCard" />
-                                                <img src={bhim5} alt="Cash" />
-                                            </span>
-                                        </li>
-                                        <li><strong>UPI Payments:</strong> Scan the QR code below to pay via any UPI-supported app.</li>
-                                        <li><strong>Verification:</strong> Always confirm the merchant name before completing the transaction.</li>
-                                        <li><strong>Receipt:</strong> You will receive a digital or printed receipt upon successful payment.</li>
-                                        <li><strong>Refunds:</strong> Refunds (if applicable) will be processed as per our refund policy within 5–7 business days.</li>
-                                        <li><strong>Support:</strong> For any payment issues, contact our billing team at <a href="mailto:www.brajesh9983693560@gmail.com">www.brajesh9983693560@gmail.com</a>.</li>
-                                    </ul>
-                                    </div>
-                                </div>
-                                <div className="section2list">
-                                    <div className="section2img">
-                                        <img
-                                            src={Truck1}
-                                            alt="Truck1"
-                                            data-aos="fade-left"
-                                            data-aos-duration="3000"
-                                            style={{borderRadius:"300px"}}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+      {/* Payment Guidelines with inline image */}
+      <div className="col-md-6">
+        <div className="section2info">
+          <div className="section2title">
+            <div className="payment-guidelines">
+              <h4>Payment Guidelines</h4>
+              <h1>Secure, Easy & Transparent Payment Options</h1>
+              <p>
+                We offer multiple secure payment methods to ensure a smooth experience. Please review the following guidelines before proceeding:
+              </p>
+
+              <ul>
+                <li><strong>Accepted Methods:</strong>
+                  <span className="icons">
+                    <img src={bhim1} alt="BHIM UPI" />
+                    <img src={bhim2} alt="UPI" />
+                    <img src={bhim3} alt="Visa" />
+                    <img src={bhim4} alt="MasterCard" />
+                    <img src={bhim5} alt="Cash" />
+                  </span>
+                </li>
+                <li><strong>UPI Payments:</strong> Scan the QR code below to pay via any UPI-supported app.</li>
+                <li><strong>Verification:</strong> Always confirm the merchant name before completing the transaction.</li>
+                <li><strong>Receipt:</strong> You will receive a digital or printed receipt upon successful payment.</li>
+                <li><strong>Refunds:</strong> Refunds (if applicable) will be processed as per our refund policy within 5–7 business days.</li>
+                <li><strong>Support:</strong> For any payment issues, contact our billing team at <a href="mailto:www.brajesh9983693560@gmail.com">www.brajesh9983693560@gmail.com</a>.</li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Truck1 Image inside same section */}
+          <div className="section2img image-inline">
+            <img
+              src={Truck1}
+              alt="Truck1"
+              data-aos="fade-left"
+              data-aos-duration="3000"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
 
